0ec93ca207869cc0aac10ec9b5cde54eda320790
Sergey <mma118@yandex.ru>
2023-02-01 13:23:45 +0800

    Added a solution task3

--- Program.cs
+++ Program.cs
@@ -15,11 +15,8 @@
 // Создать и распечатать двумерный массив со случайными целыми числами
 
 
-// // Console.Write("Bведите количество строк массива: ");
-// // int rows = int.Parse(Console.ReadLine()!);
+
 // int rows = new Random().Next(2, 7);
-// // Console.Write("Введите количество столбцов массива: ");
-// // int columns = int.Parse(Console.ReadLine()!);
 // int columns = new Random().Next(2, 7);
 // Console.WriteLine($"В массиве {rows} и {columns} столбцов:");
 
@@ -96,76 +93,149 @@
 // Программа считает сумму элементов в каждой строке и выдаёт номер строки с наименьшей суммой элементов: 1 строка
 
 
-Console.WriteLine();
+// Console.WriteLine();
 
-int rows = new Random().Next(3, 7);
-int columns = new Random().Next(3, 7);
-Console.WriteLine($"В массиве {rows} строк(и) и {columns} столбца(ов):");
+// int rows = new Random().Next(3, 7);
+// int columns = new Random().Next(3, 7);
+// Console.WriteLine($"В массиве {rows} строк(и) и {columns} столбца(ов):");
 
-Console.WriteLine();
+// Console.WriteLine();
 
-int[,] array = GetArray(rows, columns);
-PrintArray(array);
+// int[,] array = GetArray(rows, columns);
+// PrintArray(array);
 
-Console.WriteLine($"Строка с наименьшей суммой №{RowNumber(array)}");
-Console.WriteLine();
+// Console.WriteLine($"Строка с наименьшей суммой №{RowNumber(array)}");
+// Console.WriteLine();
 
-int[,] GetArray(int m, int n)
-{
-    int[,] result = new int[m, n];
+// int[,] GetArray(int m, int n)
+// {
+//     int[,] result = new int[m, n];
 
-    for (int i = 0; i < m; i++)
-    {
-        for (int j = 0; j < n; j++)
-        {
-            result[i, j] = new Random().Next(25);
-        }
-    }
-    return result;
-}
-// --- Метод определения строки с наименьшей суммой ---
+//     for (int i = 0; i < m; i++)
+//     {
+//         for (int j = 0; j < n; j++)
+//         {
+//             result[i, j] = new Random().Next(25);
+//         }
+//     }
+//     return result;
+// }
+// // --- Метод определения строки с наименьшей суммой ---
 
-void PrintArray(int[,] inArray)
-{
-    for (int i = 0; i < inArray.GetLength(0); i++)
-    {
-        for (int j = 0; j < inArray.GetLength(1); j++)
-        {
-            Console.Write($"{inArray[i, j]} ");
-        }
-        Console.WriteLine();
-    }
-    Console.WriteLine();
-}
+// void PrintArray(int[,] inArray)
+// {
+//     for (int i = 0; i < inArray.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < inArray.GetLength(1); j++)
+//         {
+//             Console.Write($"{inArray[i, j]} ");
+//         }
+//         Console.WriteLine();
+//     }
+//     Console.WriteLine();
+// }
 
-int RowNumber(int[,] array)
-{
-    int row = 0;
-    int minsum = 0;
-    for (int i = 1; i < array.GetLength(1); i++)
-    {
-        minsum += array[0, i];
-    }
-    for (int i = 0; i < array.GetLength(0); i++)
-    {
-        int sum = 0;
-        for (int j = 0; j < array.GetLength(1); j++)
-        {
-            sum += array[i, j];
-        }
-        if (minsum > sum)
-        {
-            minsum = sum;
-            row = i;
-        }
-    }
-    return row;
-}
+// int RowNumber(int[,] array)
+// {
+//     int row = 0;
+//     int minsum = 0;
+//     for (int i = 1; i < array.GetLength(1); i++)
+//     {
+//         minsum += array[0, i];
+//     }
+//     for (int i = 0; i < array.GetLength(0); i++)
+//     {
+//         int sum = 0;
+//         for (int j = 0; j < array.GetLength(1); j++)
+//         {
+//             sum += array[i, j];
+//         }
+//         if (minsum > sum)
+//         {
+//             minsum = sum;
+//             row = i;
+//         }
+//     }
+//     return row;
+// }
 
-// Задача 58: Задайте две матрицы. Напишите программу, которая будет находить произведение двух матриц.
+// Задача 58: Задайте две матрицы. Напишите программу, которая будет 
+// находить произведение двух матриц.
 // Например, даны 2 матрицы:
 // 2 4 | 3 4
 // 3 2 | 3 3
 // Результирующая матрица будет:
 // 18 20
 // 15 18
+
+
+// Console.Write("Bведите количество строк 1 массива: ");
+// int rowsA = int.Parse(Console.ReadLine()!);
+// Console.Write("Введите количество столбцов 1 массива: ");
+// int columnsA = int.Parse(Console.ReadLine()!);
+// Console.Write("Bведите количество строк 2 массива: ");
+// int rowsB = int.Parse(Console.ReadLine()!);
+// Console.Write("Введите количество столбцов 2 массива: ");
+// int columnsB = int.Parse(Console.ReadLine()!);
+
+// Console.WriteLine();
+
+// if (rowsA != columnsB)
+// {
+//     Console.WriteLine("Такие матрицы умножать нельзя");
+// }
+
+// int[,] A = GetArray(columnsA, rowsA);
+// PrintArray(A);
+
+// int[,] B = GetArray(rowsB, columnsB);
+// PrintArray(B);
+
+// PrintArray(MultMatrix(A, B));
+
+// // ---Methods---
+
+// int[,] GetArray(int m, int n)
+// {
+//     int[,] result = new int[m, n];
+
+//     for (int i = 0; i < m; i++)
+//     {
+//         for (int j = 0; j < n; j++)
+//         {
+//             result[i, j] = new Random().Next(25);
+//         }
+//     }
+//     return result;
+// }
+
+// void PrintArray(int[,] inArray)
+// {
+//     for (int i = 0; i < inArray.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < inArray.GetLength(1); j++)
+//         {
+//             Console.Write($"{inArray[i, j]} ");
+//         }
+//         Console.WriteLine();
+//     }
+//     Console.WriteLine();
+// }
+
+// // --- Метод нахождения прщизведения двух матриц ---
+
+// int[,] MultMatrix(int[,] arrayA, int[,] arrayB)
+// {
+//     int[,] arrayC = new int[arrayA.GetLength(0), arrayB.GetLength(1)];
+//     for (int i = 0; i < arrayA.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < arrayB.GetLength(1); j++)
+//         {
+//             for (int k = 0; k < arrayA.GetLength(1); k++)
+//             {
+//                 arrayC[i, j] += arrayA[i, k] * arrayB[k, j];
+//             }
+//         }
+//     }
+//     return arrayC;
+// }
