bf138dfb88c906644762f8eb065522c33902d68d
Sergey <mma118@yandex.ru>
2023-02-01 12:28:20 +0800

    Added a solution task2

--- Program.cs
+++ Program.cs
@@ -96,8 +96,71 @@
 // Программа считает сумму элементов в каждой строке и выдаёт номер строки с наименьшей суммой элементов: 1 строка
 
 
+Console.WriteLine();
 
+int rows = new Random().Next(3, 7);
+int columns = new Random().Next(3, 7);
+Console.WriteLine($"В массиве {rows} строк(и) и {columns} столбца(ов):");
 
+Console.WriteLine();
+
+int[,] array = GetArray(rows, columns);
+PrintArray(array);
+
+Console.WriteLine($"Строка с наименьшей суммой №{RowNumber(array)}");
+Console.WriteLine();
+
+int[,] GetArray(int m, int n)
+{
+    int[,] result = new int[m, n];
+
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            result[i, j] = new Random().Next(25);
+        }
+    }
+    return result;
+}
+// --- Метод определения строки с наименьшей суммой ---
+
+void PrintArray(int[,] inArray)
+{
+    for (int i = 0; i < inArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < inArray.GetLength(1); j++)
+        {
+            Console.Write($"{inArray[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+}
+
+int RowNumber(int[,] array)
+{
+    int row = 0;
+    int minsum = 0;
+    for (int i = 1; i < array.GetLength(1); i++)
+    {
+        minsum += array[0, i];
+    }
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        int sum = 0;
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            sum += array[i, j];
+        }
+        if (minsum > sum)
+        {
+            minsum = sum;
+            row = i;
+        }
+    }
+    return row;
+}
 
 // Задача 58: Задайте две матрицы. Напишите программу, которая будет находить произведение двух матриц.
 // Например, даны 2 матрицы:
